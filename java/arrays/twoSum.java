97094f7d93a2a03d3d10d63e39550d1d05512ad2
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-07-28 19:44:13 +0300

    twoSum solution

---
+++ java/arrays/twoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        // HashMap to store numbers and their indices
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+
+            // Difference needed to reach target
+            int diff = target - nums[i];
+
+            // Check if difference exists in map
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            // Store current number and its index
+            map.put(nums[i], i);
+        }
+
+        // No solution found
+        return new int[]{};
+    }
+}
